81bb26f5301eaa7df504ba5084ab1aae5432bed1
Ash MacDermott <ash.macdermott@hypergame.dev>
2021-05-26 10:29:50 -0700

    Add files via upload

---
+++ Scripts/STARTERGUI_main_SCRIPT.lua
@@ -0,0 +1,17 @@
+--[[
+# lagtest_rblx
+Open-Source Roblox lag test game
+
+---
+
+// File name: murder.lua
+// Author: AshCryptix
+// Description: Create parts to lag physics
+]]
+wait(3)
+while true do
+	local f=Instance.new("Part")
+	f.Parent = workspace.demons
+	f.Position = Vector3.new(0,27,0)
+	wait()
+end
